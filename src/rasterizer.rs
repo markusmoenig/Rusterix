784c177ac809d924a686f71a5c51d43abafe54a2
Markus Moenig <markus@moenig.io>
2025-10-23 12:10:04 +0700

    Update rasterizer.rs

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -400,7 +400,7 @@ impl Rasterizer {
                             assets,
                             None,
                             &mut execution,
-                            true,
+                            false,
                         );
                     }
 
